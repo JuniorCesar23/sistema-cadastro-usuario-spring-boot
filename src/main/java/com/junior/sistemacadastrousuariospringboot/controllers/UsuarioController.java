5d67308c4132ecddc4bf90189b73fc6f62534853
JuniorCesar23 <cesarck36@gmail.com>
2021-11-24 09:58:40 -0300

    Criacao do controller do usuario junto com post e get mapping

---
+++ src/main/java/com/junior/sistemacadastrousuariospringboot/controllers/UsuarioController.java
@@ -0,0 +1,35 @@
+package com.junior.sistemacadastrousuariospringboot.controllers;
+
+import java.util.List;
+
+import com.junior.sistemacadastrousuariospringboot.entities.Usuario;
+import com.junior.sistemacadastrousuariospringboot.services.UsuarioService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/usuarios")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<Usuario> save(@RequestBody Usuario usuario){
+        usuario = usuarioService.save(usuario);
+        return ResponseEntity.ok().body(usuario);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> findAll(){
+        List<Usuario> usuarios = usuarioService.findAll();
+        return ResponseEntity.ok().body(usuarios);
+    }
+    
+}
