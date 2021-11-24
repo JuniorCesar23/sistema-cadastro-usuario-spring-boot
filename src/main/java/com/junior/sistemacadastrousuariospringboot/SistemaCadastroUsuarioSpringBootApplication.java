775224a5ecaeebd8bf6e92e0d32be011a38ea872
JuniorCesar23 <cesarck36@gmail.com>
2021-11-24 09:35:02 -0300

    Criancao do projeto

---
+++ src/main/java/com/junior/sistemacadastrousuariospringboot/SistemaCadastroUsuarioSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.junior.sistemacadastrousuariospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaCadastroUsuarioSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaCadastroUsuarioSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/junior/sistemacadastrousuariospringboot/SistemaCadastroUsuarioSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junior.sistemacadastrousuariospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaCadastroUsuarioSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
